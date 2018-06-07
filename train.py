5cb73476b00b078c0348b97e122534ccecaa7467
abrahamryzhik <aryzhik@gmail.com>
2018-06-06 23:57:31 -0700

    added MODEL var to model_fn call

--- train.py
+++ train.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
     print('Shape of label tensor dev:', y_dev.shape)
 
     embedding_layer = create_embedding_layer(word_index)
-    model = model_fn('conv', embedding_layer)
+    model = model_fn(MODEL, embedding_layer)
 
     train_and_evaluate(model)
 
